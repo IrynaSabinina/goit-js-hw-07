94626c04f1ea18f69113cd83e752c903acbdb383
Iryna <pysarenkoiryna@gmail.com>
2022-06-18 01:05:45 +0300

    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+const galleryRef = document.querySelector(".gallery");
+const images = galleryItems.map(({ preview, description, original }) => {
+  const imgEl = document.createElement("img");
+  imgEl.type = "img";
+  imgEl.src = preview;
+  imgEl.alt = description;
+
+  return imgEl;
+});
+console.log(images);
+galleryRef.append(...images);
+
+galleryRef.addEventListener("click", onPicterClick);
+
+function onPicterClick() {
+  return (images.currentSrc = galleryItems.original);
+}
+
+// console.log(galleryRef);
 console.log(galleryItems);
